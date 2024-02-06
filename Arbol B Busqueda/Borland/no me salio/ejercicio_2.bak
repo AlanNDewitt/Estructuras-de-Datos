0be12cada1663749aa377b860cd957ca99b6d318
unknown <edgars914@gmail.com>
2024-02-06 17:53:13 -0600

    first commit

---
+++ Arbol B Busqueda/Borland/no me salio/ejercicio_2.bak
@@ -0,0 +1,277 @@
+
+#include <iostream>                                                                      //EDGAR ALAN SUAREZ VEGA
+#include <conio.h>
+#include <windows.h>
+
+using namespace std;
+
+//-------------------------------------------------------------------------------------
+void gotoxy(int x, int y)  // funcion de posicionamiento
+{
+    HANDLE hCon;
+    COORD dwPos;
+
+    dwPos.X = x;
+    dwPos.Y = y;
+    hCon = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorPosition(hCon,dwPos);
+}
+//--------------------------------------------------------------------------------------
+
+
+class Nodo 
+{
+  public :
+  
+    short dato;
+    
+    Nodo* derecha;
+    Nodo* izquierda;
+    Nodo* padre;
+    
+    Nodo()
+    {
+        dato = 0;
+        derecha = NULL;
+   		izquierda = NULL;
+    	padre = NULL;
+    }
+    
+    Nodo(short number, Nodo* father) 
+	{
+		dato = number;
+        derecha = NULL;
+   		izquierda = NULL;
+    	padre = father;
+    }
+    
+    short obtener_dato()
+    {
+    	return dato;
+	}
+	
+};
+
+
+Nodo* primer_nodo = NULL;   //el puntero del primer nodo al inicio es nulo ya que no apunta a nada todavia
+
+
+
+
+
+	short Ingresar_nodo_visual()
+	{
+		short n;
+		
+ 		system("cls");
+
+  		gotoxy(15,3);
+		cout<<" ###";
+		gotoxy(15,4);
+		cout<<"  #   #    #   ####   #####   ######   ####     ##    #####       #####     ##    #####   ####";
+		gotoxy(15,5);
+		cout<<"  #   ##   #  #    #  #    #  #       #        #  #   #    #      #    #   #  #     #    #    #";
+		gotoxy(15,6);
+		cout<<"  #   ##   #  #    #  #    #  #       #        #  #   #    #      #    #   #  #     #    #    #";
+		gotoxy(15,7);
+		cout<<"  #   # #  #  #       #    #  #####    ####   #    #  #    #      #    #  #    #    #    #    #";
+		gotoxy(15,8);
+		cout<<"  #   #  # #  #  ###  #####   #            #  ######  #####       #    #  ######    #    #    #";
+		gotoxy(15,9);
+		cout<<"  #   #   ##  #    #  #   #   #       #    #  #    #  #   #       #    #  #    #    #    #    #";
+		gotoxy(15,10);
+		cout<<" ###  #    #   ####   #    #  ######   ####   #    #  #    #      #####   #    #    #     ####";
+  		
+  		
+  		gotoxy(20,15);
+  		cout<<"Ingreso";
+  			
+  		gotoxy(20,17);
+  		cout<<"Ingresa el nuevo Numero : ";
+  		cin>>n;
+  		
+  		return n;
+		
+	}
+	
+short nodos=0;
+	
+  	void Ingresar_nodo(Nodo *&arbol,short numero, Nodo *padre)                //Funcion publica hace la operacion de ingresar al arbol nuevos nodos
+  	{	
+  		
+  		short valor_raiz;
+  			
+  		
+  		if(arbol == NULL)
+  		{
+  			Nodo *nuevo_Nodo = new Nodo(numero, padre);
+			arbol = nuevo_Nodo;
+			
+			nodos++;		
+		}
+		
+		else
+		{	
+			valor_raiz = arbol->dato;
+			
+			if (numero < valor_raiz)
+			{
+				Ingresar_nodo(arbol->izquierda,numero, arbol);
+			} 
+			
+			else
+			{
+				Ingresar_nodo(arbol->derecha,numero, arbol);	
+			}
+  		
+ 		}
+ 		
+ 		
+ 	 	
+	}
+	
+	
+
+	
+    void Mostrar_arbol(Nodo* arbol, short contador)       //Funcion que hace la operacion de imprimir arbol horizontalmente
+	{
+
+		
+		if(arbol == NULL)
+		{
+
+		}
+		
+		else
+		{
+			
+			cout<<arbol->dato<<endl;
+			
+			
+			Mostrar_arbol(arbol->izquierda, contador+0);
+			Mostrar_arbol(arbol->derecha, contador+0);
+			
+
+	
+		}		
+
+	}
+
+	
+
+void Menu()
+{
+	short option;
+	short cont=0;
+	short number;
+
+  do
+  {
+
+	system("cls");
+
+    gotoxy(70,1);
+    cout<<"* Practica 7 Arbol BB  (Nodos) *";
+
+	gotoxy(15,3);
+	cout<<" #######";
+	gotoxy(15,4);
+	cout<<" #     #  #####   ######  #####     ##     ####   #   ####   #    #  ######   ####";
+	gotoxy(15,5);
+	cout<<" #     #  #    #  #       #    #   #  #   #    #  #  #    #  ##   #  #       #";
+	gotoxy(15,6);
+	cout<<" #     #  #    #  #####   #    #  #    #  #       #  #    #  # #  #  #####    ####";
+	gotoxy(15,7);
+	cout<<" #     #  #####   #       #####   ######  #       #  #    #  #  # #  #            #";
+	gotoxy(15,8);
+	cout<<" #     #  #       #       #   #   #    #  #    #  #  #    #  #   ##  #       #    #";
+	gotoxy(15,9);
+	cout<<" #######  #       ######  #    #  #    #   ####   #   ####   #    #  ######   ####";
+
+	gotoxy(50,15);
+	cout<<"1.-Ingresar nuevo Numero";
+	gotoxy(50,17);
+	cout<<"2.-Mostrar Arbol BB";
+	gotoxy(50,19);
+	cout<<"3.-EXIT";
+	gotoxy(50,21);
+	
+    gotoxy(50,23);
+	cout<<"Option : ";
+	cin>>option;
+	
+	while(option < 1 || option > 3 )
+	{
+		gotoxy(50,26);
+		cout<<"Ingresa una opcion correcta !";
+		gotoxy(59,25);
+		cout<<"                                  ";
+		
+		gotoxy(50,25);
+		cout<<"Option : ";
+		cin>>option;
+			
+	}
+	
+	switch(option)
+	{
+		case 1 : 
+				 number = Ingresar_nodo_visual();
+				 Ingresar_nodo(primer_nodo, number , NULL);
+				 
+				 gotoxy(90,26);
+				 cout<<"Press Enter to continue";
+				 getch();  	
+				 
+				 									break;
+		
+		case 2 :	
+					system("cls");
+					cout<<"\n\nMostrar ABB \n\n";
+					
+					Mostrar_arbol(primer_nodo, cont);
+					
+					gotoxy(4,25);
+					cout<<"Arbol Binario de Busqueda con : "<<nodos<<" nodos "; 
+					
+					
+					cout<<"\t\t\t\tPress Enter to continue";
+					getch();
+					
+					cout<<"\n\n\n";
+					
+					break;
+		
+		case 3 :            break;
+	}
+  
+  
+  } while(option != 3);
+  
+}
+
+
+int main()
+{
+
+
+   Beep(500,3000);
+	
+   Menu();                            //Funcion donde se almacena practicamente todas las instrucciones, usando anidacion de Funciones
+
+   system("cls");
+   
+   gotoxy(50,15);
+   cout<<("PROGRAMA TERMINADO");
+   
+   Beep(1000,3000);
+   
+   gotoxy(80,25);
+   cout<<("-> Suarez Vega Edgar Alan <-");
+
+   
+  return 0;
+                                                                                                                                           //EDGAR ALAN SUAREZ VEGA
+}
+
+
---
+++ COLA Simple/Borland/Cola_Simple.cpp
@@ -0,0 +1,418 @@
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+
+//using namespace std;
+
+
+#define MAX 8
+
+
+
+//-------------------------------------------------------------------------------------
+void gotoxy(int x, int y)  // funcion de posicionamiento
+{
+    HANDLE hCon;
+    COORD dwPos;
+
+    dwPos.X = x;
+    dwPos.Y = y;
+    hCon = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorPosition(hCon,dwPos);
+}
+//--------------------------------------------------------------------------------------
+
+
+
+class Estructura_Cola
+{
+  public:
+
+  	short Numero[MAX];
+  	short Frente;
+  	short Final;
+  	short Estado;
+  	bool  first_push;
+
+
+
+  	void Ingreso()
+  	{
+
+  		system("cls");
+
+  		gotoxy(15,3);
+		cout<<" ######";
+		gotoxy(15,4);
+		cout<<" #     #  #    #   ####   #    #";
+		gotoxy(15,5);
+		cout<<" #     #  #    #  #       #    #";
+		gotoxy(15,6);
+		cout<<" ######   #    #   ####   ######";
+		gotoxy(15,7);
+		cout<<" #        #    #       #  #    #";
+		gotoxy(15,8);
+		cout<<" #        #    #  #    #  #    #";
+		gotoxy(15,9);
+		cout<<" #         ####    ####   #    #";
+  		
+  		if(Final == MAX - 1 )
+  		{
+  			gotoxy(30,15);
+  			cout<<"COLA SIN ESPACIOS ";
+  			
+  			gotoxy(30,17);
+  			cout<<"* OVERFLOW *";
+  			
+  			Estado = 1;
+  		
+  			gotoxy(30,20);
+  			cout<<"Press Enter to continue";
+  			getch();
+		}
+		
+		if(Final < MAX - 1 )
+		{
+
+  			if(first_push == true)
+  			{
+  				Final++;
+				gotoxy(30,15);
+  				cout<<"Ingresa un nuevo Numero : ";
+  				cin>>Numero[Final];  		
+			}			
+			
+			if(first_push == false)
+			{
+				gotoxy(30,15);
+  				cout<<"Ingresa un nuevo Numero : ";
+  				cin>>Numero[0];	
+				  
+				first_push = true;			
+			}
+
+  			
+  			Estado = 3;
+
+
+            if(Final == MAX - 1)
+  			{
+  				Estado = 1;
+			}
+  		
+  			gotoxy(30,20);
+  			cout<<"Press Enter to continue";
+  			getch();
+		}
+  	
+	}
+	
+	
+	
+	void Borrar()
+	{
+  		system("cls");
+  		
+  		gotoxy(15,3);
+		cout<<" ######";
+		gotoxy(15,4);
+		cout<<" #     #   ####   #####";
+		gotoxy(15,5);
+		cout<<" #     #  #    #  #    #";
+		gotoxy(15,6);
+		cout<<" ######   #    #  #    #";
+		gotoxy(15,7);
+		cout<<" #        #    #  #####";
+		gotoxy(15,8);
+		cout<<" #        #    #  #";
+		gotoxy(15,9);
+		cout<<" #         ####   #";
+
+
+ 		if( (Final == 0 && Numero[0] != 0)  || (Final == Frente && Final != 0) )
+  		{
+  			gotoxy(30,15);
+  			cout<<"NO PUEDES BORRAR EL UNICO NUMERO O DEJA DE SER UNA COLA ";
+
+  			gotoxy(30,17);
+  			cout<<"* Underflow *";
+
+  			Estado = 3;
+
+  			gotoxy(30,20);
+  			cout<<"Press Enter to continue";
+  			getch();
+		}
+  		
+
+
+ 		if(Final == 0 && Numero[0] == 0)
+  		{
+  			gotoxy(30,15);
+  			cout<<"COLA SIN ELEMENTOS ";
+
+  			gotoxy(30,17);
+  			cout<<"* Underflow *";
+
+  			Estado = 2;
+
+  			gotoxy(30,20);
+  			cout<<"Press Enter to continue";
+  			getch();
+		}
+
+		if(Final > 0  && Final != Frente)
+		{
+			Frente++;
+			
+  			if(Frente == MAX - 1)
+  			{
+  				Estado = 1;
+			}
+
+  			gotoxy(30,15);
+  			cout<<"Eliminado Correctamente";
+
+  			gotoxy(30,20);
+  			cout<<"Press Enter to continue";
+  			getch();
+		}
+
+
+		if(Frente > MAX-1 && Final >= MAX - 1)
+  		{
+  			gotoxy(30,15);
+  			cout<<"COLA SIN ELEMENTOS Y SIN ESPACIOS ";
+  			
+  			gotoxy(30,17);
+  			cout<<"* Underflow *";
+  			
+  			Estado = 2;
+  		
+  			gotoxy(30,20);
+  			cout<<"Press Enter to continue";
+  			getch();
+	    }
+		
+	}
+	
+	void Mostrar()
+	{
+		short y = 14;
+		
+  		system("cls");
+  		
+  		gotoxy(15,3);
+		cout<<"  #####                                #####";
+		gotoxy(15,4);
+		cout<<" #     #  #    #   ####   #    #      #     #   ####   #         ##";
+		gotoxy(15,5);
+		cout<<" #        #    #  #    #  #    #      #        #    #  #        #  #";
+		gotoxy(15,6);
+		cout<<"  #####   ######  #    #  #    #      #        #    #  #       #    #";
+		gotoxy(15,7);
+		cout<<"       #  #    #  #    #  # ## #      #        #    #  #       ######";
+		gotoxy(15,8);
+		cout<<" #     #  #    #  #    #  ##  ##      #     #  #    #  #       #    #";
+		gotoxy(15,9);
+		cout<<"  #####   #    #   ####   #    #       #####    ####   ######  #    #";
+
+
+
+		gotoxy(75,13);
+		switch(Estado)
+		{
+			case 1 :
+			cout<<"ESTADO DE LA COLA : LLENA ";
+			break;
+
+			case 2 :
+			cout<<"ESTADO DE LA COLA : VACIA ";
+			break;
+
+			case 3 :
+			cout<<"ESTADO DE LA COLA : NEUTRAL";
+			break;
+		}
+		
+		
+		gotoxy(15,12);
+		cout<<"Posicion";
+		gotoxy(25,12);
+		cout<<"Contenido";
+
+		for(short i = 0 ; i < MAX; i++)
+		{
+			gotoxy(17,y);
+			cout<<(i+1);
+			
+			if(Frente == i )
+			{
+				gotoxy(39,y);
+				cout<<("Frente");				
+			}
+			
+			if(Final == i )
+			{
+				gotoxy(39,y);
+				cout<<("Final");				
+			}
+			
+			if(Frente == i && Final ==i)
+			{
+				gotoxy(39,y);
+				cout<<("Frente y Final");
+			}
+			
+			
+					
+			gotoxy(27,y);
+			if(i >= Frente )
+			{
+				if(Numero[i] == 0)
+				{
+					cout<<"          ";
+				} 
+				
+				else
+				{
+					cout<<Numero[i];
+				}
+				
+			}
+			
+			
+
+			gotoxy(16,y+1);
+			cout<<"---------------------";
+
+			y++;
+			y++;
+		}
+
+  		y = 14;
+
+
+
+      gotoxy(75,13);
+      if( (Frente == MAX -1) && (Final == MAX-1) )
+      {
+      	cout<<"FIN DE VIDA UTIL DE LA COLA";
+      }
+
+		gotoxy(90,26);
+		cout<<"Press Enter to continue";
+		getch();
+	}
+	
+  	
+
+}Cola;
+
+
+
+
+
+void Menu()
+{
+	short option;
+
+  do
+  {
+
+	system("cls");
+
+	gotoxy(5,1);
+	//cout<<"Elementos en Pila : "<<contador;
+
+   gotoxy(80,1);
+   cout<<"* Practica 3  *";
+
+	gotoxy(15,3);
+	cout<<" #######";
+	gotoxy(15,4);
+	cout<<" #     #  #####   ######  #####     ##     ####   #   ####   #    #  ######   ####";
+	gotoxy(15,5);
+	cout<<" #     #  #    #  #       #    #   #  #   #    #  #  #    #  ##   #  #       #";
+	gotoxy(15,6);
+	cout<<" #     #  #    #  #####   #    #  #    #  #       #  #    #  # #  #  #####    ####";
+	gotoxy(15,7);
+	cout<<" #     #  #####   #       #####   ######  #       #  #    #  #  # #  #            #";
+	gotoxy(15,8);
+	cout<<" #     #  #       #       #   #   #    #  #    #  #  #    #  #   ##  #       #    #";
+	gotoxy(15,9);
+	cout<<" #######  #       ######  #    #  #    #   ####   #   ####   #    #  ######   ####";
+
+	gotoxy(50,15);
+	cout<<"1.-PUSH";
+	gotoxy(50,17);
+	cout<<"2.-POP";
+	gotoxy(50,19);
+	cout<<"3.-SHOW COLA";
+	gotoxy(50,21);
+	cout<<"4.-EXIT";
+	gotoxy(50,23);
+
+    gotoxy(50,25);
+	cout<<"Option : ";
+	cin>>option;
+	
+	while(option < 1 || option > 4 )
+	{
+		gotoxy(50,26);
+		cout<<"Ingresa una opcion correcta !";
+		gotoxy(59,25);
+		cout<<"                                  ";
+		
+		gotoxy(50,25);
+		cout<<"Option : ";
+		cin>>option;	
+	}
+	
+	switch(option)
+	{
+		case 1 : Cola.Ingreso(); break;
+		case 2 : Cola.Borrar(); break;
+		case 3 : Cola.Mostrar(); break;
+		case 4 :  break;
+	}
+  
+  
+  } while(option != 4);
+  
+}
+
+
+
+
+
+
+int main()
+{
+   Beep(500,3000);
+
+
+
+  	Cola.Frente=0;
+  	Cola.Final=0;
+   Cola.first_push = false;
+	
+   Menu();
+
+   system("cls");
+
+   gotoxy(50,15);
+   cout<<("PROGRAMA TERMINADO");
+   
+   Beep(1000,3000);
+   
+   gotoxy(80,25);
+   cout<<("-> Suarez Vega Edgar Alan <-");
+
+   getch();
+
+   
+  return 0;
+
+
+}
